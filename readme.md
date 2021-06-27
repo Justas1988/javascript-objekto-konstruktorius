51ab8dcd0a8fc48a4915c2014f697d2cc8927fb7
Justas1988 <b.justas@outlook.com>
2021-06-27 23:40:04 +0300

    initial-commit

---
+++ readme.md
@@ -0,0 +1,55 @@
+![LICENSE](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
+![Security Status](https://img.shields.io/security-headers?label=Security&url=https%3A%2F%2Fgithub.com&style=flat-square)
+![Gluten Status](https://img.shields.io/badge/Gluten-Free-green.svg)
+![Eco Status](https://img.shields.io/badge/ECO-Friendly-green.svg)
+![Alco Status](https://img.shields.io/badge/Contains-Alcohol-red.svg)
+
+# Namų darbai Javascript "Objekto konstruktorius"
+
+_29 group_
+
+This project is for educational porpuses only. Best viewed on 16:9 aspect ratio monitors, Full HD or higher resolution. Why copy when you can think yourself?
+Make mistakes and improve!
+
+Javascript "Objekto konstruktorius"
+
+ 
+
+1. Susikurkite objektą "Klase", konstruktoriaus kurti šiam objektui nereikia.
+2. Sukurkite 20 objektų masyvą, kuriame yra objektai "Studentai". Objektą sukurkite pasinaudodami konstruktoriumi.
+"Studentas" konstruktorius turi sukurti tokį objektą:
+*id - skaičius
+*vardas - tekstas
+*pavarde - tekstas
+*pazymys - skaičius
+*lankomumamas - masyvas, 20 elementų, užpildytas "x" ir "n" reikšmėmis.
+*data - šios dienos data.
+3. Studentų objektų masyvą patalpikinti į objektą "Klase".
+4. Objekte "Klase", sukurkite metodą, kuris išvestų bendrą studentų pažymių vidurkį.
+
+ 
+
+Papildoma:
+Išveskite lankomumo duomenis į HTML lentelę. Lentelė gali būti <table> žymė arba sudėti lentelę iš div elementų, pasinaudojant display: table galimybėmis. Pirmas stulpelis: studento vardas ir pavardė, likę stulpeliai "x" ir "n" reikšmės.
+ 
+Pastaba: Papildoma užduotis gali priversti pasukti galvą, ji nėra privaloma.
+
+
+## Project features
+
+- Github pages
+- Desktop only
+- Html
+- CSS
+- JavaScript
+- Functions
+- Objects
+- Methods
+- Arrays
+- Loops
+- Conditions
+- Constructors
+
+## Authors
+
+[Justas Belevičius](https://github.com/Justas1988)
---
+++ script.js
@@ -0,0 +1,92 @@
+// Javascript "Objekto konstruktorius" 
+
+// 1. Susikurkite objektą "Klase", konstruktoriaus kurti šiam objektui nereikia.
+// 2. Sukurkite 20 objektų masyvą, kuriame yra objektai "Studentai". Objektą sukurkite pasinaudodami konstruktoriumi.
+// "Studentas" konstruktorius turi sukurti tokį objektą:
+// *id - skaičius
+// *vardas - tekstas
+// *pavarde - tekstas
+// *pazymys - skaičius
+// *lankomumamas - masyvas, 20 elementų, užpildytas "x" ir "n" reikšmėmis.
+// *data - šios dienos data.
+// 3. Studentų objektų masyvą patalpikinti į objektą "Klase".
+// 4. Objekte "Klase", sukurkite metodą, kuris išvestų bendrą studentų pažymių vidurkį. 
+
+// Papildoma:
+// Išveskite lankomumo duomenis į HTML lentelę. Lentelė gali būti <table> žymė arba sudėti lentelę iš div elementų, pasinaudojant display: table galimybėmis. Pirmas stulpelis: studento vardas ir pavardė, likę stulpeliai "x" ir "n" reikšmės.
+ 
+// Pastaba: Papildoma užduotis gali priversti pasukti galvą, ji nėra privaloma.
+
+"use strict"
+
+
+
+//konstruktorius Studentas 
+
+function Studentas(id, vardas, pavarde, pazymys, lankomumas, data) {
+    this.id = id;
+    this.vardas = vardas;
+    this.pavarde = pavarde;
+    this.pazymys = pazymys;
+    this.lankomumas = lankomumas
+    this.data = data;
+
+};
+
+//lankomumo masyvo funkcija, N ir X reikšmes generuoja atsitiktinai
+
+function lankomumas() {
+const lankomumoMasyvas = [20];
+for (let i=0; i < 20; i++) {
+    let randomNumber = Math.floor(Math.random() * 100);
+    if (randomNumber % 2 == 0) {
+        lankomumoMasyvas[i] = "N";        
+    } else {
+        lankomumoMasyvas[i] = "X";        
+    }
+}
+return lankomumoMasyvas;
+};
+
+// console.log(lankomumas());
+
+//funkcija studentųGeneratorius, generuoja 20 studentų pagal konstruktorių Studentas
+
+function studentuGeneratorius() {
+    const studentuMasyvas = [20];
+    const date = new Date();
+    for (let i = 0; i < 20; i++) {
+        let pazymys = (Math.floor(Math.random()*10)+1);
+        studentuMasyvas[i] = new Studentas(i+1, "Studentas" + (i+1), "Studentukas" + (i+1), pazymys, lankomumas(), date);
+    }
+    return studentuMasyvas;
+};
+
+// console.log(studentuGeneratorius());
+
+//objektas klase su studentų masyvu
+
+const klase = {
+    studentai: [],
+
+    pazymiuVidurkis: function() {
+        let suma = 0;
+        for (let i in this.studentai[0]) {
+            suma = suma + this.studentai[0][i].pazymys;
+        }
+        let vidurkis = suma / this.studentai[0].length;
+        return vidurkis;
+    }
+};
+klase.studentai.push(studentuGeneratorius());
+
+console.log(klase);
+// console.log(klase.studentai[0][2]);
+console.log(klase.pazymiuVidurkis());
+
+
+
+
+
+
+
